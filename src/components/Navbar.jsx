5d8e2caa9b1dce36b88240621f14e86f954253ee
jade0819 <break0819@gmail.com>
2023-03-12 22:04:11 +0900

    Admin: admin 사용자만 상품 추가(NewProduct) 메뉴 보이게 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,7 @@ export default function Navbar() {
   const [user, setUser] = useState();
 
   useEffect(() => {
-    // onUserStateChange(setUser);
     onUserStateChange((user) => {
-      console.log(user);
       setUser(user);
     });
   }, []);
@@ -25,9 +23,11 @@ export default function Navbar() {
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         <Link to="/carts">Carts</Link>
-        <Link to="/products/new" className="text-2xl">
-          <BsFillPencilFill />
-        </Link>
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
         {user && <User user={user} />}
         {!user && <button onClick={login}>Login</button>}
         {user && <button onClick={logout}>Logout</button>}
